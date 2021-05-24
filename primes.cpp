a00e0cd91e0d6527a1e3498dad13af375604e58b
SamRennich <samuelrennich@gmail.com>
2021-05-24 16:33:16 -0400

    finished prime seive, now able to run up to 1 000 000 000 quite quickly

--- primes.cpp
+++ primes.cpp
@@ -1,60 +1,85 @@
+// Prime Seive
+
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 // Constants
-const int range = 1000; // Sets the upper limit of where to find primes
+const unsigned long long int range = 1000000; // Sets the upper limit of where to find primes
 
 // Prototypes
-std::vector<int> createVector(); // Creates the vector with numbers from 0 to range
-void primeSeive(std::vector<int>); // Removes all non-primes leaving only primes in the vector
+void primeSeive(std::vector<bool>&); // Sets all non-prime values to false
+void printPrimes(std::vector<bool>); // Prints all values that are prime given prime vector
+int numOfPrimes(std::vector<bool>); // Finds the number of primes in the vector
 
 int main() {
 	
 	// Create initial vector
-	std::vector<int> primes = createVector();
+	std::vector<bool> primes(range + 1, true);
+	primes[0] = false; // Manual override for value 0
+	primes[1] = false; // Manual override for value 1
 	
-	// Sort, leaving only primes
-	//primeSeive(primes);
+	// Sort, leaving only primes true
+	primeSeive(primes);
 	
-	// Print vector
-	for (int i = 0; i < (primes.size() - 1); i++) {
-		std::cout << primes[i] << ", ";
-	}
-	std::cout << primes[primes.size() - 1] << std::endl;
-
+	// Prints all primes in the given range
+	//printPrimes(primes);
+	
+	// Print the number of primes in range
+	std::cout << numOfPrimes(primes) << std::endl;
+	
 	//Exit
 	std::cin.clear();
     getchar();
     return 0;
 }
 
-// Creates the vector with numbers from 0 to range
-std::vector<int> createVector() {
+
+// Removes all non-primes leaving only primes as true in the vector
+void primeSeive(std::vector<bool>& primes) {
 	
-	std::vector<int> primes;
+	// Sets scale to the square root of the range for finding prime divisors
+	unsigned long long int scale = static_cast<int>(sqrt(range));
 	
-	for (int i = 0; i < range; i++) {
-		primes[i] = i + 1;
+	// Turns all even numbers (bar 2) to false
+	for (unsigned long long int i = 4; i <= range; i += 2) {
+		primes[i] = false;
 	}
 	
-	return primes; // Finish
-}
-
-// Removes all non-primes leaving only primes in the vector
-void primeSeive(std::vector<int> primes) {
-	
-	for (int i = 0; i < (range / 2); i++) {
-		
-		int j = i + 1;
-		
-		while (j < primes.size()) {
-			if (primes[j] % primes[i] == 0) {
-				primes.erase(primes.begin() + j);
-			} else {
-				j++;
+	// Iterates through every other value in the vector
+	for (unsigned long long int i = 3; i <= scale; i += 2) {
+		if (primes[i]) { // If the value is prime...
+			for (unsigned long long int j = (i * 2); j <= range; j += i) { // Set all multiples of it to be false
+				primes[j] = false;
 			}
 		}
 	}
 	
 	return; // Finish
+}
+
+// Prints all values that are prime
+void printPrimes(std::vector<bool> primes) {
+	
+	for (unsigned long long int i = 0; i <= range; i++) { // Iterates through every value in the vector
+		if (primes[i]) { // If it is prime...
+			std::cout << i << std::endl; // Print it
+		}
+	}
+	
+	return; // Finish
+}
+
+// Finds the number of primes in the vector
+int numOfPrimes(std::vector<bool> primes) {
+	
+	unsigned long long int counter = 0; // Counts the number of primes
+	
+	for (unsigned long long int i = 0; i < primes.size(); i++) { // Iterates through every value in the vector
+		if (primes[i]) { // If it is prime...
+			counter++; // Add to the counter
+		}
+	}
+	
+	return counter; // Finish
 }
