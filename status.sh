84a97251a1268349f9ebb581b447a862f8bc1b03
SamRennich <samuelrennich@gmail.com>
2021-11-03 17:12:51 -0400

    hopefully final version, uses simplified form with basic key

--- status.sh
+++ status.sh
@@ -2,26 +2,55 @@
 
 # Check every repository for status
 
+# Key
+# P = Unpushed Changes
+# C = Uncommitted Changes
+# S = Unstaged Changes
+
 # Text Colors
 NC=$(tput sgr0)
 RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
-YELLOW=$(tput setaf 3)
-CYAN=$(tput setaf 6)
 
 # For every dir...
 for d in */; do
   cd $d # Move to dir
 
-  # Checking scheme
-  if [ -n "$(git cherry)" ]; then # If there are unpushed changes...
-    printf "%-20s %s\n" $d "${CYAN}Unpushed Changes${NC}"
-  elif [ -n "$(git diff --cached)" ]; then # If there are uncommitted changes...
-    printf "%-20s %s\n" $d "${YELLOW}Uncommitted Changes${NC}"
-  elif [ -n "$(git status --porcelain)" ]; then # If there are unstaged changes...
-    printf "%-20s %s\n" $d "${RED}Unstaged Changes${NC}"
-  else # If there are no changes...
-    printf "%-20s %s\n" $d "${GREEN}No Changes${NC}"
+  # Status Conditions
+  STATUS=""
+
+  # Unchanged flag
+  FLAG=true
+
+  # If there are unpushed changes...
+  if [ -n "$(git cherry)" ]; then
+    STATUS+="P"
+    FLAG=false
+  else
+    STATUS+="."
+  fi
+
+  # If there are uncommitted changes...
+  if [ -n "$(git diff --cached)" ]; then
+    STATUS+="C"
+    FLAG=false
+  else
+    STATUS+="."
+  fi
+
+  # If there are unstaged changes...
+  if [ -n "$(git status --porcelain)" ]; then
+    STATUS+="S"
+    FLAG=false
+  else
+    STATUS+="."
+  fi
+
+  # If there are no changes...
+  if [ "${FLAG}" = true ]; then
+    printf "%s %s\n" $STATUS "${GREEN}${d}${NC}"
+  else
+    printf "%s %s\n" $STATUS "${RED}${d}*${NC}"
   fi
 
   cd .. # Move back
