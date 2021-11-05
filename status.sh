df483f2f907f5aaa8ac526dc34657327c8a5b194
SamRennich <samuelrennich@gmail.com>
2021-11-04 20:09:40 -0400

    finally finished with status.sh

--- status.sh
+++ status.sh
@@ -11,7 +11,7 @@
 NORMAL=$(tput sgr0)
 RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
-CYAN=$(tput setaf 6)
+YELLOW=$(tput setaf 3)
 
 # For every dir...
 for d in */; do
@@ -21,13 +21,13 @@ for d in */; do
   STATUS=""
 
   # If there are unpushed changes...
-  if [ -n "$( git status | fgrep 'push' )" ]; then STATUS+="${CYAN}P${NORMAL}"; fi
+  if [ -n "$( git status | fgrep 'push' )" ]; then STATUS+="${GREEN}P${NORMAL}"; fi
 
   # If there are uncommitted changes...
-  if [ -n "$( git status | fgrep 'committed' )" ]; then STATUS+="${GREEN}C${NORMAL}"; fi
+  if [ -n "$( git status | fgrep 'to be committed' )" ]; then STATUS+="${YELLOW}C${NORMAL}"; fi
 
   # If there are unstaged changes...
-  if [ -n "$( git status | fgrep 'staged' )" ]; then STATUS+="${RED}S${NORMAL}"; fi
+  if [ -n "$( git status | fgrep 'not staged' )" ]; then STATUS+="${RED}S${NORMAL}"; fi
 
   # Print dir name and git status -s
   if [ -n "${STATUS}" ]; then printf "%-3s %s\n" $STATUS $d; fi
