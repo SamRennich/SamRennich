7995fe0999d148be0c3b3f56969ea9c5280f5d1a
SamRennich <samuelrennich@gmail.com>
2021-11-04 20:06:52 -0400

    almost finished

--- status.sh
+++ status.sh
@@ -4,25 +4,33 @@
 
 # Key
 # P = Unpushed Changes
-# M = Uncommitted or Unstaged Changes
+# C = Uncommitted Changes
+# S = Unstaged Changes
 
 # Text Colors
 NORMAL=$(tput sgr0)
 RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
 CYAN=$(tput setaf 6)
 
 # For every dir...
 for d in */; do
   cd $d # Move to dir
 
-  # If there are unpushed changes...
-  if [ -n "$(git cherry)" ]; then STATUS+="${CYAN}P${NORMAL}"; fi
+  # Store status flags
+  STATUS=""
 
-  # If there are uncommitted or unstaged changes...
-  if [ -n "$(git status --porcelain)" ]; then STATUS+="${RED}M${NORMAL}"; fi
+  # If there are unpushed changes...
+  if [ -n "$( git status | fgrep 'push' )" ]; then STATUS+="${CYAN}P${NORMAL}"; fi
+
+  # If there are uncommitted changes...
+  if [ -n "$( git status | fgrep 'committed' )" ]; then STATUS+="${GREEN}C${NORMAL}"; fi
+
+  # If there are unstaged changes...
+  if [ -n "$( git status | fgrep 'staged' )" ]; then STATUS+="${RED}S${NORMAL}"; fi
 
   # Print dir name and git status -s
-  if [ -n "${STATUS}" ]; then printf "%-2s %s\n" "${STATUS}" "${d}"; fi
+  if [ -n "${STATUS}" ]; then printf "%-3s %s\n" $STATUS $d; fi
 
   cd .. # Move back
 done
