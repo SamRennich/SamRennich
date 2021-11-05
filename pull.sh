6be36155e63e62840a5192fd993c3898a6be98c4
SamRennich <samuelrennich@gmail.com>
2021-11-04 20:00:07 -0400

    correcte git pull mostly

--- pull.sh
+++ pull.sh
@@ -2,16 +2,19 @@
 
 # Pull from origin for every repository
 
+# Key
+# U = Updates are Available
+
 # Text Colors
 NORMAL=$(tput sgr0)
 RED=$(tput setaf 1)
-GREEN=$(tput setaf 2)
-CYAN=$(tput setaf 6)
 
 # For every dir...
 for d in */; do
   cd $d # Move to dir
-  git pull --quiet # Pull without printing
-  printf "%-20s %s\n" $d "${GREEN}Up to Date${NC}"
+
+  # If out of date...
+  if [ -n "$(git remote show origin | grep 'local out of date')" ]; then printf "%s %s\n" "${RED}U${NORMAL}" $d; fi
+
   cd .. # Move back
 done
